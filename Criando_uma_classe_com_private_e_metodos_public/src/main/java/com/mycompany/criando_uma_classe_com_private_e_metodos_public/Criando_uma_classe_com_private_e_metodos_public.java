4c10d255c3a4e38808b69ba448f813f62469d71c
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-02-28 18:52:23 -0300

    finalizado

---
+++ Criando_uma_classe_com_private_e_metodos_public/src/main/java/com/mycompany/criando_uma_classe_com_private_e_metodos_public/Criando_uma_classe_com_private_e_metodos_public.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.criando_uma_classe_com_private_e_metodos_public;
+
+/**
+ *
+ * @author User
+ */
+public class Criando_uma_classe_com_private_e_metodos_public {
+
+    public static void main(String[] args) {
+        Produto objproduto = new Produto();
+        objproduto.setNome("lapis");
+        objproduto.setPreco(1.20);
+        objproduto.status();
+
+    }
+}

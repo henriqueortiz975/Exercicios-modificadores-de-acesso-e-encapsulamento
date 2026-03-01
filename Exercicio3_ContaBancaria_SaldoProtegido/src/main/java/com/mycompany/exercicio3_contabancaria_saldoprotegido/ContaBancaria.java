6c2a7eefa45d276e5cef0be4bce232185ff3e27b
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-02-28 22:18:49 -0300

    finalizado

--- Exercicio3_ContaBancaria_SaldoProtegido/src/main/java/com/mycompany/exercicio3_contabancaria_saldoprotegido/ContaBancaria.java
+++ Exercicio3_ContaBancaria_SaldoProtegido/src/main/java/com/mycompany/exercicio3_contabancaria_saldoprotegido/ContaBancaria.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.exercicio3_contabancaria_saldoprotegido;
 
-/**
- *
- * @author User
- */
 public class ContaBancaria {
-    
+    public String titular;
+    private double saldo;
+
+    public double getSaldo(){
+        System.out.println("conta: " + titular);
+        System.out.println("saldo: " + saldo);
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+
+    public void depositar(int depositar){
+        saldo += depositar;
+        System.out.println("a quantidade que foi depositada: " + depositar);
+    }
+
+    public void sacar(int sacar){
+        System.out.println("a quantidade do saque requirido foi: " + sacar);
+        if(saldo > sacar){
+            System.out.println("seu saque de: " + sacar + " foi completo.");
+            saldo -= sacar;
+            System.out.println("seu saldo de agora é: " + saldo);
+        }else{
+            System.out.println("seu saque de: " + sacar + " foi recusada por ser acima do valor.");
+        }
+    }
+
 }
--- Exercicio3_ContaBancaria_SaldoProtegido/src/main/java/com/mycompany/exercicio3_contabancaria_saldoprotegido/Exercicio3_ContaBancaria_SaldoProtegido.java
+++ Exercicio3_ContaBancaria_SaldoProtegido/src/main/java/com/mycompany/exercicio3_contabancaria_saldoprotegido/Exercicio3_ContaBancaria_SaldoProtegido.java
@@ -1,16 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.exercicio3_contabancaria_saldoprotegido;
 
-/**
- *
- * @author User
- */
 public class Exercicio3_ContaBancaria_SaldoProtegido {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ContaBancaria objconta = new ContaBancaria();
+        objconta.titular = "Henrique Ibarra Ortiz Urbano";
+        objconta.setSaldo(134.50);
+        objconta.getSaldo();
+        objconta.depositar(120);
+        objconta.sacar(200);
+
     }
+
 }

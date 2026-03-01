6ce3bfa9244978a07f24ebea4fe52a05342750d9
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-02-28 22:16:40 -0300

    finalizado

---
+++ Exercicio3_ContaBancaria_SaldoProtegido/src/main/java/com/mycompany/exercicio3_contabancaria_saldoprotegido/Exercicio3_ContaBancaria_SaldoProtegido.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio3_contabancaria_saldoprotegido;
+
+/**
+ *
+ * @author User
+ */
+public class Exercicio3_ContaBancaria_SaldoProtegido {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

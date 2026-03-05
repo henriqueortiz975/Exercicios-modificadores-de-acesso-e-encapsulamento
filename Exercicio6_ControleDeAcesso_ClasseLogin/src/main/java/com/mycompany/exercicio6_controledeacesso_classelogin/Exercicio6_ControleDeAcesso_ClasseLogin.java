e841feb1ee8b8d3859954aa7b4278807e9eb9344
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-03-04 21:18:01 -0300

    finalizado

---
+++ Exercicio6_ControleDeAcesso_ClasseLogin/src/main/java/com/mycompany/exercicio6_controledeacesso_classelogin/Exercicio6_ControleDeAcesso_ClasseLogin.java
@@ -0,0 +1,11 @@
+package com.mycompany.exercicio6_controledeacesso_classelogin;
+
+public class Exercicio6_ControleDeAcesso_ClasseLogin {
+
+    public static void main(String[] args) {
+        Usuario objconta = new Usuario();
+        System.out.println("o login foi feito corretamente"
+        + objconta.autenticar("Henrique Ibarra", "senha123"));
+
+    }
+}
---
+++ Exercicio6_ControleDeAcesso_ClasseLogin/src/main/java/com/mycompany/exercicio6_controledeacesso_classelogin/Usuario.java
@@ -0,0 +1,19 @@
+package com.mycompany.exercicio6_controledeacesso_classelogin;
+
+public class Usuario {
+    private String login = "Henrique Ibara";
+    private String senha = "senha123";
+    
+    
+    public boolean autenticar(String login, String senha){
+          if(this.login == login && this.senha == senha){           
+              return true;
+          }else{
+              return false;
+          } 
+    }
+    
+    public void getAutenticar(){
+        
+    }
+}

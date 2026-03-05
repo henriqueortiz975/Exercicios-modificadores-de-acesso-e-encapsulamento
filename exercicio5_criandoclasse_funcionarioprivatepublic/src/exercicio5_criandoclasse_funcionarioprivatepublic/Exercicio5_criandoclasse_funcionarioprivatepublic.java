141ac4ddd6cb8c9509d64b515fd8501e73010e9a
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-03-05 16:21:16 -0300

    finalizado

--- exercicio5_criandoclasse_funcionarioprivatepublic/src/exercicio5_criandoclasse_funcionarioprivatepublic/Exercicio5_criandoclasse_funcionarioprivatepublic.java
+++ exercicio5_criandoclasse_funcionarioprivatepublic/src/exercicio5_criandoclasse_funcionarioprivatepublic/Exercicio5_criandoclasse_funcionarioprivatepublic.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio5_criandoclasse_funcionarioprivatepublic;
 
-/**
- *
- * @author aluno.saolucas
- */
 public class Exercicio5_criandoclasse_funcionarioprivatepublic {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Funcionario objfuncionario = new Funcionario();
+        objfuncionario.setNome("Henrique Ibarra");
+        objfuncionario.setSalario(1343.30);
+        
+        System.out.println("Funcionario: " + objfuncionario.getNome());
+        System.out.println("Salario: " + objfuncionario.getSalario());
+
     }
     
 }
